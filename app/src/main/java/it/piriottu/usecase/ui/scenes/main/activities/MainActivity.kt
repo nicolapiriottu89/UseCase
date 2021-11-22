511a48fe1485184a1b6646a432ca48d64fe6b444
Piriottu <nicola.piriottu@overapp.it>
2021-11-22 15:33:43 +0100

    working progress

--- app/src/main/java/it/piriottu/usecase/ui/scenes/main/activities/MainActivity.kt
+++ app/src/main/java/it/piriottu/usecase/ui/scenes/main/activities/MainActivity.kt
@@ -1,15 +1,13 @@
 package it.piriottu.usecase.ui.scenes.main.activities
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.navigation.NavArgumentBuilder
-import androidx.navigation.fragment.NavHostFragment
-import it.piriottu.usecase.R
-import it.piriottu.usecase.ui.scenes.main.viewmodels.PostsFragmentViewModel
+import androidx.activity.viewModels
 import it.piriottu.usecase.databinding.ActivityMainBinding
+import it.piriottu.usecase.managers.SessionManager
 import it.piriottu.usecase.ui.scenes.main.adapters.PostsListAdapter
+import it.piriottu.usecase.ui.scenes.main.viewmodels.PostsFragmentViewModel
 
 /**
  * Created by OverApp on 15/11/21.
@@ -23,15 +21,57 @@ class MainActivity : AppCompatActivity() {
      */
     private lateinit var binding: ActivityMainBinding
 
+    /**
+     * ViewModel
+     * */
+    private val viewModel: PostsFragmentViewModel by viewModels()
 
     /**
-     * Nav Controller
+     * Adapter
      */
+    private val adapter by lazy {
+        PostsListAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Setup
+        setupObserver()
+        //TODO copy your token here
+        SessionManager.userToken = "COPY-YOUR-TOKEN-HERE"
+
+        //Setup recyclerView
+        //binding.recyclerView.adapter = adapter
+
+        //Get posts
+        viewModel.getPosts()
     }
+
+    private fun setupObserver() {
+        // Use Case
+        viewModel.useCaseLiveData.observe(this, {
+            it.getContentIfNotHandled()?.let { useCase ->
+              //  binding.progress.isVisible = false
+                when (useCase) {
+
+                    is PostsFragmentViewModel.UseCaseLiveData.ShowPosts -> {
+                        // Update Items
+                        adapter.submitList(useCase.items)
+
+                        Log.d("MainActivity", "ShowItems ${useCase.items}")
+                    }
+
+                    is PostsFragmentViewModel.UseCaseLiveData.Error -> {
+                        Log.d("MainActivity", "Error ${useCase.code}")
+                    }
+
+                }
+            }
+        })
+    }
+
 
 }
--- app/src/main/java/it/piriottu/usecase/ui/scenes/main/fragments/PostsFragment.kt
+++ app/src/main/java/it/piriottu/usecase/ui/scenes/main/fragments/PostsFragment.kt
@@ -6,11 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import it.piriottu.usecase.databinding.FragmentMainBinding
 import it.piriottu.usecase.databinding.FragmentPostsBinding
 import it.piriottu.usecase.managers.SessionManager
 import it.piriottu.usecase.ui.scenes.main.adapters.PostsListAdapter
